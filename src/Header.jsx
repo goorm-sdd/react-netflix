a757259e7144c31a91d2f2c7b8d33cec88134e54
ssoogit <ssoogit02@gmail.com>
2025-05-02 06:53:08 +0900

    feat(header): 헤더 디자인 설정

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,18 @@
+import './Header.css';
+
 const Header = () => {
   return (
-    <>
-      <div>
-        <button>
-          <img src="./src/assets/netflix-icon.png" alt="Netflix"></img>
-        </button>
-        <button>TV Shows</button>
-        <button>Movies</button>
-      </div>
-    </>
+    <div className="Header">
+      <button>
+        <img
+          src="./src/assets/netflix-icon.png"
+          alt="Netflix"
+          style={{ width: '40px' }}
+        ></img>
+      </button>
+      <button>TV Shows</button>
+      <button>Movies</button>
+    </div>
   );
 };
 
