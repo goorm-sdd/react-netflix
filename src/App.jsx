f9377525d3eb35257fb487318bde280a99902e91
projectmiluju <dnjsdyd0712@naver.com>
2025-05-02 13:37:49 +0900

    docs: 파일 경로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
-import Header from './Header';
-import Footer from './Footer';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
