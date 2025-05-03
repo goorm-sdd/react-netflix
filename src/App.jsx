0211d7a3184aa0967db790f5fefd60edc3ff167c
cherish0-0 <jiyu01160@naver.com>
2025-05-04 01:18:43 +0900

    feat: mylist 버튼 클릭시 페이지 이동하도록 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import { BrowserRouter, Outlet, Routes, Route } from 'react-router-dom';
+import MyList from './pages/MyList';
 
-function App() {
+function Layout() {
   return (
     <>
       <Header />
-      <div></div>
+      <Outlet />
       <Footer />
     </>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/MyList" element={<MyList />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
