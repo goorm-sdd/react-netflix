92327b97b88038c9ce5dbb8f37cb3660220b6520
sarajeong17 <sarajeong17@gmail.com>
2025-05-03 16:47:06 +0900

    feat: tv, 검색,detail 추가

--- src/api/requests.js
+++ src/api/requests.js
@@ -8,6 +8,19 @@ export const requests = {
   fetchHorrorMovies: '/discover/movie?with_genres=27',
   fetchRomanceMovies: '/discover/movie?with_genres=10749',
   fetchDocumentaries: '/discover/movie?with_genres=99',
-  fetchMovieDetails: (movieId) => `/movie/${movieId}`,
+
+  // TV
+  fetchActionAdventureTV: '/discover/tv?with_genres=10759',
+  fetchComedyTV: '/discover/tv?with_genres=35',
+  fetchDocumentaryTV: '/discover/tv?with_genres=99',
+  fetchDramaTV: '/discover/tv?with_genres=18',
+  fetchRealityTV: '/discover/tv?with_genres=10764',
+
+  // 검색
   fetchSearchMovies: (query) => `/search/movie?query=${query}`,
+  fetchSearchTV: (query) => `/search/tv?query=${query}`,
+
+  // 디테일
+  fetchMovieDetails: (movieId) => `/movie/${movieId}`,
+  fetchTVDetails: (tvId) => `/tv/${tvId}`,
 };
