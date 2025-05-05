ffa4d02828d6b25deb81a30452843b5e0200b155
projectmiluju <dnjsdyd0712@naver.com>
2025-05-05 17:53:22 +0900

    feat: detail modal UI movie만 구형

--- src/components/DetailModal/DetailModal.jsx
+++ src/components/DetailModal/DetailModal.jsx
@@ -45,23 +45,23 @@ export default function DetailModal({ isOpen, onClose, movieId }) {
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal-content" onClick={(e) => e.stopPropagation()}>
         <img className="modal-poster" src={movie.image} alt={movie.title} />
-
-        <div className="modal-buttons">
-          <div className="icon-button">
-            <img src={MyListIcon} alt="My List" className="icon-image" />
-            <p>My List</p>
+        <div className="modal-detail">
+          <div className="modal-buttons">
+            <div className="icon-button">
+              <img src={MyListIcon} alt="My List" className="icon-image" />
+              <p>My List</p>
+            </div>
+            <div className="icon-button">
+              <img src={ShareIcon} alt="Share" className="icon-image" />
+              <p>Share</p>
+            </div>
           </div>
-          <div className="icon-button">
-            <img src={ShareIcon} alt="Share" className="icon-image" />
-            <p>Share</p>
+          <div className="modal-body">
+            <p className="modal-subtitle">{movie.subtitle}</p>
+            <h2 className="modal-title">{movie.title}</h2>
+            <p className="modal-description">{movie.description}</p>
+            <p className="modal-tags">{movie.tags}</p>
           </div>
-        </div>
-
-        <div className="modal-body">
-          <p className="modal-subtitle">{movie.subtitle}</p>
-          <h2 className="modal-title">{movie.title}</h2>
-          <p className="modal-description">{movie.description}</p>
-          <p className="modal-tags">{movie.tags}</p>
         </div>
       </div>
     </div>
