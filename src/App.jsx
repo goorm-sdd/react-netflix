caffe90146d6b3bfdc5760969ccb50f3f5038681
ssoogit <ssoogit02@gmail.com>
2025-05-06 08:23:54 +0900

    feat(logo): app.jsx에 연결
    우선 따로 정리 없이 반영함

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Logo from './pages/Logo/Logo';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Banner from './components/Banner/Banner';
@@ -6,6 +7,7 @@ import Banner from './components/Banner/Banner';
 function App() {
   return (
     <>
+      <Logo />
       <Header />
       <Banner />
       <Footer />
