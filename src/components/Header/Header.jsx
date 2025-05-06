e3ba830cc17dc9189f2156738a654b457d297faf
cherish0-0 <jiyu01160@naver.com>
2025-05-06 21:53:25 +0900

    feat: 현재 경로에 따라서 헤더 다르게 나오도록 구현
    - mylist 페이지에서는 mylist 안에서 선택 가능하게 기존 버튼 2개 삭제 및 새 버튼 추가
    - arrow-down.png 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,10 @@
 import './Header.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
+  const location = useLocation();
+  const isMyListPage = location.pathname === '/MyList';
+
   return (
     <div className="Header">
       <Link to="/">
@@ -13,8 +16,25 @@ const Header = () => {
           ></img>
         </button>
       </Link>
-      <button>TV Shows</button>
-      <button>Movies</button>
+
+      {!isMyListPage && (
+        <>
+          <button>TV Shows</button>
+          <button>Movies</button>
+        </>
+      )}
+      {isMyListPage && (
+        <>
+          <button>
+            <img
+              src="./src/assets/arrow-down.png"
+              alt="arrow-down"
+              style={{ width: '22px' }}
+            ></img>
+            <div>My List</div>
+          </button>
+        </>
+      )}
     </div>
   );
 };
