6669175f9497e30e6f2fedd9bfae8181d8c4b679
cherish0-0 <jiyu01160@naver.com>
2025-05-06 20:18:18 +0900

    feat: 넷플릭스 로고 클릭시 베이스 url로 되돌아가도록 함

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,18 @@
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className="Header">
-      <button>
-        <img
-          src="./src/assets/netflix-icon.png"
-          alt="Netflix"
-          style={{ width: '40px' }}
-        ></img>
-      </button>
+      <Link to="/">
+        <button>
+          <img
+            src="./src/assets/netflix-icon.png"
+            alt="Netflix"
+            style={{ width: '40px' }}
+          ></img>
+        </button>
+      </Link>
       <button>TV Shows</button>
       <button>Movies</button>
     </div>
