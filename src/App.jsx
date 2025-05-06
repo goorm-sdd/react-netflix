541c426c3845b25d0950d425f2e796d005430ec6
ssoogit <ssoogit02@gmail.com>
2025-05-07 06:05:13 +0900

    refactor: 라우터 파일 생성 후 app.jsx의 라우팅 관련 코드 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,32 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import AppRoutes from './routes/AppRoutes';
 import Intro from './pages/Intro/Intro';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import Banner from './components/Banner/Banner';
-import DetailModal from './components/DetailModal/DetailModal';
-import { useState } from 'react';
-import { BrowserRouter, Outlet, Routes, Route } from 'react-router-dom';
-import MyList from './pages/MyList';
-
-function Main({ openModal }) {
-  return (
-    <>
-      <Intro />
-      <Banner onPreviewClick={openModal} onInfoClick={openModal} />
-    </>
-  );
-}
 
 function App() {
-  const [selectedId, setSelectedId] = useState(null);
-  const [selectedType, setSelectedType] = useState(null);
-  const [isOpen, setIsOpen] = useState(false);
+  const [showIntro, setShowIntro] = useState(true);
 
-  const openModal = (id, type) => {
-    setSelectedId(id);
-    setSelectedType(type);
-    setIsOpen(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowIntro(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleIntroClick = () => {
+    setShowIntro(false);
   };
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <Layout
-              isOpen={isOpen}
-              setIsOpen={setIsOpen}
-              selectedId={selectedId}
-              selectedType={selectedType}
-            />
-          }
-        >
-          <Route index element={<Main openModal={openModal} />} />
-          <Route path="/MyList" element={<MyList />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-function Layout({ isOpen, setIsOpen, selectedId, selectedType }) {
-  return (
     <>
-      <Header />
-      <Outlet />
-      <Footer />
-      <DetailModal
-        isOpen={isOpen}
-        onClose={() => setIsOpen(false)}
-        movieId={selectedId}
-        movieType={selectedType}
-      />
+      {showIntro ? (
+        <div onClick={handleIntroClick}>
+          <Intro />
+        </div>
+      ) : (
+        <AppRoutes />
+      )}
     </>
   );
 }
