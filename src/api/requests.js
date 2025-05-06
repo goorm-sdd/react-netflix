ce6ef55bd92dc1eb027cfef05b3b2ed0416c77b8
sarajeong17 <sarajeong17@gmail.com>
2025-05-07 06:46:33 +0900

    feat: popular api 추가

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,8 +1,11 @@
 export const requests = {
   fetchNowPlaying: 'movie/now_playing',
-  fetchNetflixOriginals: '/discover/tv?with_networks=213',
+  fetchPopular: 'movie/popular',
+  fetchNetflixOriginals: '/discover/movie?with_networks=213',
   fetchTrending: '/trending/all/week',
   fetchTopRated: '/movie/top_rated',
+  fetchReleaseDates: '/movie/{movie_id}/release_dates',
+  fetchWatchlist: 'account/{account_id}/watchlist/movies',
   fetchActionMovies: '/discover/movie?with_genres=28',
   fetchComedyMovies: '/discover/movie?with_genres=35',
   fetchHorrorMovies: '/discover/movie?with_genres=27',
