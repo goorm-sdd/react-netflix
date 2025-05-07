6e334c0e35c9824492008012b156bb042a22239f
이홍재 <astroboy3488@gmail.com>
2025-05-07 21:17:50 +0900

    chore: 파일추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,34 @@
-import { useState, useEffect } from 'react';
 import './App.css';
-import AppRoutes from './routes/AppRoutes';
-import Intro from './pages/Intro/Intro';
+import Logo from './pages/Logo/Logo';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Banner from './components/Banner/Banner';
+import DetailModal from './components/DetailModal/DetailModal';
+import { useState } from 'react';
 
 function App() {
-  const [showIntro, setShowIntro] = useState(true);
+  const [selectedId, setSelectedId] = useState(null);
+  const [selectedType, setSelectedType] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowIntro(false);
-    }, 2000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-  const handleIntroClick = () => {
-    setShowIntro(false);
+  const openModal = (id, type) => {
+    setSelectedId(id);
+    setSelectedType(type);
+    setIsOpen(true);
   };
 
   return (
     <>
-      {showIntro ? (
-        <div onClick={handleIntroClick}>
-          <Intro />
-        </div>
-      ) : (
-        <AppRoutes />
-      )}
+      <Logo />
+      <Header />
+      <Banner onPreviewClick={openModal} onInfoClick={openModal} />
+      <Footer />
+      <DetailModal
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        movieId={selectedId}
+        movieType={selectedType}
+      />
     </>
   );
 }
