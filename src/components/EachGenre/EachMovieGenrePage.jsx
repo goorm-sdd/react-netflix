3d8caf1c43793d7cfc83747c0afb58a96bce36a7
ssoogit <ssoogit02@gmail.com>
2025-05-07 22:08:10 +0900

    feat(genre): 무한 스크롤 구현

--- src/components/EachGenre/EachMovieGenrePage.jsx
+++ src/components/EachGenre/EachMovieGenrePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import { instance } from '../../api/axios';
 import { requests } from '../../api/requests';
@@ -6,55 +6,87 @@ import { requests } from '../../api/requests';
 const EachMovieGenrePage = () => {
   const { genreName } = useParams();
   const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const observer = useRef();
+
+  const lastMovieElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading],
+  );
 
   useEffect(() => {
     const fetchMovies = async () => {
       try {
+        setLoading(true);
         let request;
 
         switch (genreName) {
           case 'action':
-            request = requests.fetchActionMovies;
+            request = `${requests.fetchActionMovies}&page=${page}`;
             break;
           case 'comedy':
-            request = requests.fetchComedyMovies;
+            request = `${requests.fetchComedyMovies}&page=${page}`;
             break;
           case 'horror':
-            request = requests.fetchHorrorMovies;
+            request = `${requests.fetchHorrorMovies}&page=${page}`;
             break;
           case 'romance':
-            request = requests.fetchRomanceMovies;
+            request = `${requests.fetchRomanceMovies}&page=${page}`;
             break;
           case 'documentary':
-            request = requests.fetchDocumentaries;
+            request = `${requests.fetchDocumentaries}&page=${page}`;
             break;
           default:
             return;
         }
 
         const response = await instance.get(request);
-        setMovies(response.data.results);
+        setMovies((prevMovies) => [...prevMovies, ...response.data.results]);
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching movies:', error);
+        setLoading(false);
       }
     };
 
     fetchMovies();
-  }, [genreName]);
+  }, [genreName, page]);
 
   return (
     <>
-      {movies.map(
-        (movie) =>
-          movie.poster_path && (
+      {movies.map((movie, index) => {
+        if (movie.poster_path && index === movies.length - 1) {
+          return (
+            <div ref={lastMovieElementRef} key={movie.id} className="grid-item">
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+              />
+            </div>
+          );
+        } else if (movie.poster_path) {
+          return (
             <div key={movie.id} className="grid-item">
               <img
                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                 alt={movie.title}
               />
             </div>
-          ),
-      )}
+          );
+        }
+        return null;
+      })}
+      {loading && <div>Loading...</div>}
     </>
   );
 };
--- src/components/EachGenre/EachTVGenrePage.jsx
+++ src/components/EachGenre/EachTVGenrePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
 import { instance } from '../../api/axios';
 import { requests } from '../../api/requests';
@@ -6,55 +6,91 @@ import { requests } from '../../api/requests';
 const EachTVGenrePage = () => {
   const { genreName } = useParams();
   const [tvShows, setTVShows] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const observer = useRef();
+
+  const lastTVShowElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading],
+  );
 
   useEffect(() => {
     const fetchTVShows = async () => {
       try {
+        setLoading(true);
         let request;
 
         switch (genreName) {
           case 'action-adventure':
-            request = requests.fetchActionAdventureTV;
+            request = `${requests.fetchActionAdventureTV}&page=${page}`;
             break;
           case 'comedy':
-            request = requests.fetchComedyTV;
+            request = `${requests.fetchComedyTV}&page=${page}`;
             break;
           case 'documentary':
-            request = requests.fetchDocumentaryTV;
+            request = `${requests.fetchDocumentaryTV}&page=${page}`;
             break;
           case 'drama':
-            request = requests.fetchDramaTV;
+            request = `${requests.fetchDramaTV}&page=${page}`;
             break;
           case 'reality':
-            request = requests.fetchRealityTV;
+            request = `${requests.fetchRealityTV}&page=${page}`;
             break;
           default:
             return;
         }
 
         const response = await instance.get(request);
-        setTVShows(response.data.results);
+        setTVShows((prevTVShows) => [...prevTVShows, ...response.data.results]);
+        setLoading(false);
       } catch (error) {
         console.error('Error fetching TV shows:', error);
+        setLoading(false);
       }
     };
 
     fetchTVShows();
-  }, [genreName]);
+  }, [genreName, page]);
 
   return (
     <>
-      {tvShows.map(
-        (tvShow) =>
-          tvShow.poster_path && (
+      {tvShows.map((tvShow, index) => {
+        if (tvShow.poster_path && index === tvShows.length - 1) {
+          return (
+            <div
+              ref={lastTVShowElementRef}
+              key={tvShow.id}
+              className="grid-item"
+            >
+              <img
+                src={`https://image.tmdb.org/t/p/w500${tvShow.poster_path}`}
+                alt={tvShow.name}
+              />
+            </div>
+          );
+        } else if (tvShow.poster_path) {
+          return (
             <div key={tvShow.id} className="grid-item">
               <img
                 src={`https://image.tmdb.org/t/p/w500${tvShow.poster_path}`}
-                alt={tvShow.title}
+                alt={tvShow.name}
               />
             </div>
-          ),
-      )}
+          );
+        }
+        return null;
+      })}
+      {loading && <div>Loading...</div>}
     </>
   );
 };
