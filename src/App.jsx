c4e3d118494575bd7b5140289a7855ed371e6868
이홍재 <astroboy3488@gmail.com>
2025-05-08 00:15:25 +0900

    feat: 배열 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,49 @@
+import { useState, useEffect } from 'react';
 import './App.css';
-import Logo from './pages/Logo/Logo';
+import AppRoutes from './routes/AppRoutes';
+import Intro from './pages/Intro/Intro';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Banner from './components/Banner/Banner';
 import DetailModal from './components/DetailModal/DetailModal';
-import { useState } from 'react';
+import MainPage from './pages/MainPage';
+import { BrowserRouter, Outlet, Routes, Route } from 'react-router-dom';
+import MyList from './pages/MyList/MyList';
+
+function Main({ openModal }) {
+  return (
+    <>
+      <Intro />
+      <Banner onPreviewClick={openModal} onInfoClick={openModal} />
+      <MainPage />
+    </>
+  );
+}
 
 function App() {
-  const [selectedId, setSelectedId] = useState(null);
-  const [selectedType, setSelectedType] = useState(null);
-  const [isOpen, setIsOpen] = useState(false);
+  const [showIntro, setShowIntro] = useState(true);
 
-  const openModal = (id, type) => {
-    setSelectedId(id);
-    setSelectedType(type);
-    setIsOpen(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowIntro(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const handleIntroClick = () => {
+    setShowIntro(false);
   };
 
   return (
     <>
-      <Logo />
-      <Header />
-      <Banner onPreviewClick={openModal} onInfoClick={openModal} />
-      <Footer />
-      <DetailModal
-        isOpen={isOpen}
-        onClose={() => setIsOpen(false)}
-        movieId={selectedId}
-        movieType={selectedType}
-      />
+      {showIntro ? (
+        <div onClick={handleIntroClick}>
+          <Intro />
+        </div>
+      ) : (
+        <AppRoutes />
+      )}
     </>
   );
 }
